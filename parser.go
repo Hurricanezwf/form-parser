386a411c67e91574a4d0a86ab27e712c19a06b48
Hurricanezwf <1094646850@qq.com>
2018-07-14 13:16:49 +0800

    新增对[]byte的解析

---
+++ parser.go
@@ -0,0 +1,287 @@
+package formparser
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+const pkgName = "formparser"
+
+// FormParser 将结构体对象转换成HTTP请求所需的KV形式
+// 只处理struct及*struct类型
+type FormParser struct {
+	// 用于转换的tag名字, 类似于json序列化的json tag
+	tag string
+
+	// 用于忽略转换的符号, 类似于json序列化的"-"
+	ignoreFlag string
+
+	// 编码器
+	encoders map[reflect.Kind]kindEncoder
+}
+
+func Default() *FormParser {
+	return NewFormParser("zwf", "-")
+}
+
+func NewFormParser(tag, ignoreFlag string) *FormParser {
+	if len(tag) <= 0 {
+		panic(fmt.Sprintf("%s: Missing `tag` value", pkgName))
+	}
+	if len(ignoreFlag) <= 0 {
+		panic(fmt.Sprintf("%s: Missing `ignoreFlag` value", pkgName))
+	}
+	p := FormParser{
+		tag:        tag,
+		ignoreFlag: ignoreFlag,
+	}
+	return p.init()
+}
+
+// ToMap the param v should be either reflect.ValueOf(struct) or reflect.ValueOf(*struct)
+func (p *FormParser) ToMap(v reflect.Value) (map[string]string, error) {
+	kvs, err := p.parse(v)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]string)
+	for _, kv := range kvs {
+		m[kv.K] = kv.V
+	}
+	return m, err
+}
+
+func (p *FormParser) Debug(v reflect.Value) {
+	kvs, err := p.parse(v)
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, kv := range kvs {
+		fmt.Printf("%10s : %s\n", kv.K, kv.V)
+	}
+}
+
+func (p *FormParser) parse(rv reflect.Value) ([]KV, error) {
+	for rv.Kind() != reflect.Struct {
+		if rv.Kind() == reflect.Ptr {
+			rv = rv.Elem()
+			continue
+		}
+		return nil, errors.New("Param obj is invalid, struct or non-nil *struct is needed")
+	}
+
+	var kvs []KV
+	for i := 0; i < rv.NumField(); i++ {
+		// 过滤掉缺省的数据
+		field := rv.Field(i)
+		for field.Kind() == reflect.Ptr {
+			field = field.Elem() // 消除指针
+		}
+		if field.Kind() == reflect.Invalid {
+			continue
+		}
+		// 过滤掉指定标签的数据
+		tagK, drop := p.fieldTag(rv.Type().Field(i))
+		if drop {
+			continue
+		}
+
+		// 获取字段值
+		kvs = append(kvs, p.encode(field, tagK)...)
+	}
+	return kvs, nil
+}
+
+func (p *FormParser) fieldTag(f reflect.StructField) (tag string, drop bool) {
+	tag = f.Tag.Get(p.tag)
+	switch tag {
+	case p.ignoreFlag:
+		drop = true
+	case "":
+		tag = f.Name
+	}
+	return tag, drop
+}
+
+func (p *FormParser) encode(v reflect.Value, tagK string) []KV {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem() // 消除指针
+	}
+
+	e, ok := p.encoders[v.Kind()]
+	if !ok || e == nil {
+		panic(fmt.Sprintf("Unknown type %v", v.Kind()))
+	}
+	return e(v, tagK)
+}
+
+func (p *FormParser) init() *FormParser {
+	p.encoders = map[reflect.Kind]kindEncoder{
+		reflect.String:     p.encodeString,
+		reflect.Bool:       p.encodeBool,
+		reflect.Int:        p.encodeInt,
+		reflect.Int8:       p.encodeInt8,
+		reflect.Int16:      p.encodeInt16,
+		reflect.Int32:      p.encodeInt32,
+		reflect.Int64:      p.encodeInt64,
+		reflect.Uint:       p.encodeUint,
+		reflect.Uint8:      p.encodeUint8,
+		reflect.Uint16:     p.encodeUint16,
+		reflect.Uint32:     p.encodeUint32,
+		reflect.Uint64:     p.encodeUint64,
+		reflect.Float32:    p.encodeFloat32,
+		reflect.Float64:    p.encodeFloat64,
+		reflect.Complex64:  p.encodeComplex64,
+		reflect.Complex128: p.encodeComplex128,
+		reflect.Slice:      p.encodeSlice,
+		reflect.Array:      p.encodeSlice,
+		reflect.Struct:     p.encodeStruct,
+		reflect.Map:        p.encodeMap,
+		reflect.Invalid:    p.encodeInvalid,
+	}
+	return p
+}
+
+func (p *FormParser) encodeString(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, v.Interface().(string)})
+}
+
+func (p *FormParser) encodeBool(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatBool(v.Interface().(bool))})
+}
+
+func (p *FormParser) encodeInt(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.Itoa(v.Interface().(int))})
+}
+
+func (p *FormParser) encodeInt8(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatInt(int64(v.Interface().(int8)), 10)})
+}
+
+func (p *FormParser) encodeInt16(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatInt(int64(v.Interface().(int16)), 10)})
+}
+
+func (p *FormParser) encodeInt32(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatInt(int64(v.Interface().(int32)), 10)})
+}
+
+func (p *FormParser) encodeInt64(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatInt(v.Interface().(int64), 10)})
+}
+
+func (p *FormParser) encodeUint(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatUint(uint64(v.Interface().(uint)), 10)})
+}
+
+func (p *FormParser) encodeUint8(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatUint(uint64(v.Interface().(uint8)), 10)})
+}
+
+func (p *FormParser) encodeUint16(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatUint(uint64(v.Interface().(uint16)), 10)})
+}
+
+func (p *FormParser) encodeUint32(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatUint(uint64(v.Interface().(uint32)), 10)})
+}
+
+func (p *FormParser) encodeUint64(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, strconv.FormatUint(v.Interface().(uint64), 10)})
+}
+
+func (p *FormParser) encodeFloat32(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, fmt.Sprintf("%v", v.Interface().(float32))})
+}
+
+func (p *FormParser) encodeFloat64(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, fmt.Sprintf("%v", v.Interface().(float64))})
+}
+
+func (p *FormParser) encodeComplex64(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, fmt.Sprintf("%v", v.Interface().(complex64))})
+}
+
+func (p *FormParser) encodeComplex128(v reflect.Value, tagK string) (rt []KV) {
+	return append(rt, KV{tagK, fmt.Sprintf("%v", v.Interface().(complex128))})
+}
+
+func (p *FormParser) encodeSlice(v reflect.Value, tagK string) (rt []KV) {
+	// 如果是[]byte，则进行base64后做成KV
+	b, isBytes := v.Interface().([]byte)
+	if isBytes == true {
+		rt = append(rt, KV{tagK, base64.StdEncoding.EncodeToString(b)})
+		return rt
+	}
+	// 如果是非[]byte，则将每个元素单独做成KV
+	for i := 0; i < v.Len(); i++ {
+		rt = append(rt, p.encode(v.Index(i), fmt.Sprintf("%s.%d", tagK, i))...)
+	}
+	return rt
+}
+
+func (p *FormParser) encodeStruct(v reflect.Value, tagK string) (rt []KV) {
+	kvs, err := p.parse(v)
+	if err != nil {
+		panic(fmt.Sprintf("Parse value for tagK(%s) failed, %v", tagK, err))
+	}
+	for i, kv := range kvs {
+		kv.K = tagK + "." + kv.K
+		kvs[i] = kv
+	}
+	rt = kvs
+	return rt
+}
+
+func (p *FormParser) encodeMap(v reflect.Value, tagK string) (rt []KV) {
+	keys := v.MapKeys()
+	for _, k := range keys {
+		keyPair := p.encode(k, "")
+		valPair := p.encode(v.MapIndex(k), "")
+		for _, key := range keyPair {
+			for _, val := range valPair {
+				rt = append(rt, KV{
+					K: tagK + "." + key.V,
+					V: val.V,
+				})
+			}
+		}
+	}
+	return rt
+}
+
+func (p *FormParser) encodeInvalid(v reflect.Value, tagK string) (rt []KV) {
+	// do nothing
+	return nil
+}
+
+type kindEncoder func(v reflect.Value, tagK string) (rt []KV)
+
+type KV struct {
+	K string
+	V string
+}
+
+func StringPtr(v string) *string {
+	return &v
+}
+
+func IntPtr(v int) *int {
+	return &v
+}
+
+func Int64Ptr(v int64) *int64 {
+	return &v
+}
+
+func Float64Ptr(v float64) *float64 {
+	return &v
+}
+
+func BoolPtr(v bool) *bool {
+	return &v
+}
---
+++ parser_test.go
@@ -0,0 +1,65 @@
+package formparser
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var h = Hello{
+	A: 1,
+	B: "BB",
+	C: 2,
+	D: 3.14,
+	E: []int{2, 0, 32},
+	F: Info{CPU: StringPtr("1核")},
+	G: true,
+	H: []*Info{
+		&Info{CPU: StringPtr("2核")},
+		&Info{CPU: StringPtr("3核")},
+		&Info{CPU: StringPtr("4核")},
+	},
+	I: map[string]*string{
+		"m1": StringPtr("m1"),
+		"m2": StringPtr("m2"),
+	},
+	J: []byte("Golang"),
+}
+
+func TestParse(t *testing.T) {
+	p := NewFormParser("a", "-")
+	_, err := p.parse(reflect.ValueOf(h))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	p.Debug(reflect.ValueOf(h))
+}
+
+func BenchmarkParse(b *testing.B) {
+	p := NewFormParser("a", "-")
+	for i := 0; i < b.N; i++ {
+		_, err := p.parse(reflect.ValueOf(h))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+	p.Debug(reflect.ValueOf(h))
+}
+
+type Hello struct {
+	A int                `a:"a"`
+	B string             `a:"b"`
+	C int64              `a:"c"`
+	D float64            `a:"d"`
+	E []int              `a:"e"`
+	F Info               `a:"f"`
+	G bool               `a:"g"`
+	H []*Info            `a:"h"`
+	I map[string]*string `a:"i"`
+	J []byte             `a:"j"`
+}
+
+type Info struct {
+	CPU *string `a:"cpu"`
+}
